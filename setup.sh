385b8769d4852bb90810f7104d9eb8d95e116f87
NAME <mail@gmail.com>
2025-07-07 19:48:15 +0300

    unneccesary packages removed.

--- setup.sh
+++ setup.sh
@@ -6,10 +6,6 @@ pkg install viu
 pkg install openssh
 pkg install python 
 pkg install termux-api
-pkg install tilde
-pkg install vi
-pkg install nano
-pkg install htop
 
 chmod +x ./*.sh
 source configuration.sh
